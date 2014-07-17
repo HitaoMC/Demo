d49fda542a3042213331927469cf3086b2c7370e
HitaoMC <30952781@qq.com>
2014-07-17 15:31:20 +0800

    新建工程

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main
+{
+    
+    /** <一句话功能简述>
+     * <功能详细描述>
+     * @param args [参数说明]
+     * 
+     * @return void [返回类型说明]
+     * @exception throws [违例类型] [违例说明]
+     * @see [类、类#方法、类#成员]
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
